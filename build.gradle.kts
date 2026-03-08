94098f0ae06e1bb3bb4f7ea13b8187d619cd2a26
Mario <7338138+kaeron-dev@users.noreply.github.com>
2026-03-08 03:43:36 -0300

    chore: initial monorepo setup with gradle root (#12)

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    kotlin("jvm") version "2.0.21" apply false
+    kotlin("plugin.spring") version "2.0.21" apply false
+    id("org.springframework.boot") version "3.3.5" apply false
+    id("io.spring.dependency-management") version "1.1.6" apply false
+}
+
+allprojects {
+    group = "com.rcaagent"
+    version = "1.0.0"
+
+    repositories {
+        mavenCentral()
+    }
+}
